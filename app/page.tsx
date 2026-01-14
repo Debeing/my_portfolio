b5d412b8efcf10aab08e7e65b93aa647a0706672
Debeing <mbaitelameric@gmail.com>
2026-01-14 11:21:31 +0000

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,25 +9,25 @@ import Contact from "@/components/ui/Contact";
 import Footer from "@/components/ui/Footer";
 import { useState } from "react";
 import { ToastContainer } from "react-toastify";
-import ParticlesBackground from "@/components/Particule";
-import ParticlesBackgroundPoint from "@/components/ParticulePoint";
+
+
 
 export default function Home() {
   const [langue, setLangue] = useState<string>("Anglais")
 
   return (
-    <div className="w-full overflow-x-hidden">
+    <div className="min-h-screen overflow-x-hidden">
       <Navbar langue={langue} setLangue={setLangue} />
 
       {/* Hero avec particules en lignes */}
       <section className="relative min-h-screen">
-        <ParticlesBackground />  {/* lignes */}
+       
         <Featured langue={langue} />
       </section>
 
       {/* About avec particules en points */}
       <section className="relative min-h-screen">
-        <ParticlesBackgroundPoint />  {/* points */}
+       
         <AboutPro langue={langue} />
       </section>
 
